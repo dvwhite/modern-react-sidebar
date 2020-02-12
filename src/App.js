b3c8525088cacd4bc4cb459bfd8ff5ccdb72ac46
David White <dlw8e@hscmail.mcc.virginia.edu>
2020-02-12 16:08:23 -0500

    Create app using create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SideBar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from'@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+function SideBar() {
+    return (
+        <List disablePadding dense>
+            <ListItem button>
+                <ListItemText>Home</ListItemText>
+            </ListItem>
+            <ListItem button>
+                <ListItemText>Billing</ListItemText>
+            </ListItem>
+            <ListItem button>
+                <ListItemText>Settings</ListItemText>
+            </ListItem>
+        </List>
+    )
+}
+
+export default SideBar;
