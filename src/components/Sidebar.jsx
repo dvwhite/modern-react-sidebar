03ea72a019c7d74b74bbe6d712c0f02aba3a7504
David White <dlw8e@hscmail.mcc.virginia.edu>
2020-02-14 13:04:26 -0500

    Split sidebar into subcomponent SideBarItem and use recursively to render submenu items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,10 +7,28 @@ function SideBar({ items }) {
     return (
         <div className='sidebar'>
             <List disablePadding dense>
-                {items.map(({ label, name, ...rest }) => (
-                    <ListItem key={name} button {...rest}>
-                        <ListItemText>{label}</ListItemText>
-                    </ListItem>
+                {items.map(({ label, name, items: subItems, ...rest }) => (
+                    <React.Fragment key={name}>
+                        <ListItem style={{ paddingLeft: 18 }} key={name} button {...rest}>
+                            <ListItemText>{label}</ListItemText>
+                        </ListItem>
+                        {Array.isArray(subItems) ? (
+                            <List disablePadding>
+                                {subItems.map((subItem) => (
+                                    <ListItem 
+                                        key={subItem.name} 
+                                        style={{ paddingLeft: 36 }}
+                                        button
+                                        dense
+                                    >
+                                        <ListItemText className="sidebar-item-text">
+                                            {subItem.label}
+                                        </ListItemText>
+                                    </ListItem>
+                                ))}
+                            </List>
+                        ) : null}
+                    </React.Fragment>
                 ))}           
             </List>
         </div>
